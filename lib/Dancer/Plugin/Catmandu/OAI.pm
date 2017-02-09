c35b7dbc36601a5f5df94eb22a4d4e58d1a25df5
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2017-02-09 12:08:30 +0100

    0.0310

--- lib/Dancer/Plugin/Catmandu/OAI.pm
+++ lib/Dancer/Plugin/Catmandu/OAI.pm
@@ -6,7 +6,7 @@ Dancer::Plugin::Catmandu::OAI - OAI-PMH provider backed by a searchable Catmandu
 
 =cut
 
-our $VERSION = '0.0309';
+our $VERSION = '0.0310';
 
 use Catmandu::Sane;
 use Catmandu::Util qw(:is);
